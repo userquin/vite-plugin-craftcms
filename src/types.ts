e322edc42610f7f2c7bf5c82713c437e56efa7ce
Joaquín Sánchez <userquin@gmail.com>
2022-05-08 15:12:49 +0200

    update types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import { HTMLElement } from "node-html-parser";
+import { ResolvedConfig } from "vite";
 
 export interface ParsedHtml {
   head: HTMLElement[];
@@ -9,4 +10,30 @@ export interface TemplateParams extends Partial<ParsedHtml> {
   basePath?: string;
   proxyUrl?: string;
   mode?: string;
+}
+
+export interface CmsCraftPluginOptions {
+  /**
+   * @default Infinity
+   */
+  concurrency?: number
+  /**
+   * @default './templates/_partials/vite.twig'
+   */
+  outputFile?: string
+  /**
+   * The template to apply.
+   */
+  template?: (templateParams: TemplateParams) => string
+  /**
+   * @default 'http://localhost'
+   */
+  devServerBaseAddress?: string
+}
+
+// internal resolved options: all options required except concurrency
+export interface ResolvedCmsCraftOptions extends Required<Exclude<CmsCraftPluginOptions, 'concurrency'>> {
+  config: ResolvedConfig
+  basePath: string
+  proxyUrl: string
 }
